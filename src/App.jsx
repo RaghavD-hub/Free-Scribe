123c0b489979299a342788291e28b799b1822798
Raghav Dobriyal <dobriyalraghav10@gmail.com>
2025-07-27 10:40:32 +0530

    committed

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import { useState, useRef, useEffect } from 'react'
 import HomePage from "./components/HomePage"
 import Header from "./components/Header"
 import FileDisplay from './components/FileDisplay'
+import Transcribing from './components/Transcribing'
+import Information from './components/Information'
 
 function App() {
   const [file, setFile] = useState(null)  
   const [audioStream, setAudioStream] = useState(null)
+  const [output, setOutput] = useState(null)
+  
+  const [loading, setLoading] = useState(true)
 
   const isAudioAvailable = file || audioStream
 
@@ -14,22 +19,23 @@ function App() {
     setAudioStream(null)
   }
 
-  return (
-    <>
-      <div className="flex flex-col max-w-[1000px] mx-auto w-full">
-        <section className="min-h-screen flex flex-col">
-          <Header /> 
-          {isAudioAvailable ? (
-            <FileDisplay handleAudioReset={handleAudioReset} file={file} audioStream={setAudioStream} />
-          ): (
-            <HomePage setFile = {setFile} setAudioStream = {setAudioStream} />
-          )}
-          </section>
-        <h1 className="text-green-300">hello</h1>
-        <footer></footer>
-      </div>
-    </>
+   return (
+    <div className='flex flex-col max-w-[1000px] mx-auto w-full'>
+      <section className='min-h-screen flex flex-col'>
+        <Header />
+        {output ? (
+          <Information output={output} finished={finished}/>
+        ) : loading ? (
+          <Transcribing />
+        ) : isAudioAvailable ? (
+          <FileDisplay handleAudioReset = {handleAudioReset} file = {file} audioStream = {setAudioStream} />
+        ) : (
+          <HomePage setFile={setFile} setAudioStream={setAudioStream} />
+        )}
+      </section>
+      <footer></footer>
+    </div>
   )
-} 
+}
 
 export default App  
